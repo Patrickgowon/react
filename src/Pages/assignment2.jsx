47f871526a8e635c7030f2ec52b7f92444bea7cf
patrick <patrickgowon18@gmail.com>
2025-05-29 11:15:25 -0700

    update

--- src/Pages/assignment2.jsx
+++ src/Pages/assignment2.jsx
@@ -4,7 +4,7 @@ const Numbercount = () =>{
     const [num, setNum] = useState(1)
     
     return(
-        <div className="p-10 bg-gray-300 w-[32rem] mt-8 mb-8 flex gap-5">
+        <div className="ml-10 p-10 bg-gray-300 w-[30rem] mt-8 mb-8 flex gap-5">
             <button onClick={() => setNum( num +1)} className="p-2 bg-blue-700 text-white">increase</button>
             <p className="text-xl text-red-400">{num}</p>
             <button onClick={() => setNum(num -1)} className="p-2 bg-blue-700 text-white">Decrease</button>
